b35aa3d2ec269d966a900c8d53a618fc55578f8a
Joaquin Pozzo <pozzojoa@gmail.com>
2023-08-21 16:30:15 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import Navbar from "./componentes/Navbar/Navbar"
-import Layout from "./componentes/Layout/Layout"
-import Hero from "./componentes/Hero/Hero"
-import Categories from "./componentes/Categories/Categories"
-import Products from "./componentes/Products/Products"
-import Footer from "./componentes/Footer/Footer"
 import Routes from "./routes/Routes"
 
 function App() {
